413ce2ae9094031b55b3920e6ed4efafc9f47bd1
irabudkova <thehive@list.ru>
2020-09-30 20:54:02 +0300

    feat: add reverse-int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let result = 0;
+
+    if (n < 0) {
+        n *= -1;
+    }
+
+    while (n > 0) {
+        a = n % 10;
+        result = result * 10 + a
+        n = Math.floor(n / 10);
+    }
+    return result;
 }
